6e92f38b91fc5e9076d19a960cee65d3f4aa70e7
Thierry Seegers <surfintheusa@yahoo.com>
2021-04-05 13:02:49 +0200

    Bring over vector implementation from exerccie 3.

---
+++ lib/vector/vector_api.c
@@ -0,0 +1,229 @@
+#include "vector_api.h"
+
+#include "vector_types.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void grow(
+    vector* v,
+    size_t const needed)
+{
+    if(v->capacity == 0)
+    {
+        v->capacity = needed;
+        v->data = malloc(v->capacity * v->element_size);
+    }
+    else
+    {
+        while(v->capacity <= needed)
+        {
+            v->capacity *= v->growth_factor;
+            v->data = realloc(v->data, v->capacity * v->element_size);
+        }
+    }
+}
+
+vector make_vector(
+    size_t const element_size,
+    size_t const num_elements,
+    float const growth_factor)
+{
+    return (vector){
+            .element_size = element_size,
+            .growth_factor = growth_factor,
+            .data = num_elements ? malloc(num_elements * element_size) : NULL,
+            .size = num_elements,
+            .capacity = num_elements,
+            };
+}
+
+void destroy(
+    vector* v)
+{
+    free(v->data);
+    v->data = NULL;
+}
+
+size_t size(
+    vector const v)
+{
+    return v.size;
+}
+
+size_t capacity(
+    vector const v)
+{
+    return v.capacity;
+}
+
+void push_back(
+    vector* v,
+    void const* data)
+{
+    insert(v, end(v), data);
+}
+
+void* pop_back(
+    vector* v)
+{
+    erase(v, end(v));
+    return v->data + v->size * v->element_size;
+}
+
+void insert(
+    vector* v,
+    iterator const i,
+    void const* data)
+{
+    size_t const chunk_offset = i.element - v->data;     // En octets.
+    size_t const chunk_size = end(v).element - i.element;    // En octets.
+    
+    grow(v, v->size + 1);
+
+    memmove(v->data + chunk_offset + v->element_size, v->data + chunk_offset, chunk_size);
+    memcpy(v->data + chunk_offset, data, v->element_size);
+    ++v->size;
+}
+
+void erase(
+    vector* v,
+    iterator const i)
+{
+    size_t const chunk_offset = i.element - v->data;     // En octets.
+    size_t const chunk_size = end(v).element - i.element;    // En octets.
+
+    memmove(v->data + chunk_offset, v->data + chunk_offset + v->element_size, chunk_size);
+    --v->size;
+}
+
+void assign(
+    vector* v,
+    iterator first,
+    iterator last)
+{
+    v->size = (last.element - first.element) / v->element_size;
+    grow(v, v->size);
+
+    memcpy(v->data, first.element, last.element - first.element);
+}
+
+void clear(
+    vector* v)
+{
+    v->size = 0;
+}
+
+void swap(
+    vector* a,
+    vector* b)
+{
+    if(a != b)
+    {
+        void *t = b->data;
+        b->data = a->data;
+        a->data = t;
+
+        a->size ^= b->size;
+        b->size ^= a->size;
+        a->size ^= b->size;
+
+        a->capacity ^= b->capacity;
+        b->capacity ^= a->capacity;
+        a->capacity ^= b->capacity;
+    }
+}
+
+void reserve(
+    vector* v,
+    size_t const num_elements)
+{
+    grow(v, num_elements);
+}
+
+void resize(
+    vector* v,
+    size_t const num_elements)
+{
+    if(v->size < num_elements)
+    {
+        v->size = v->capacity = num_elements;
+        v->data = realloc(v->data, v->capacity * v->element_size);
+    }
+    else if(v->size > num_elements)
+    {
+        v->size = num_elements;
+    }
+}
+
+void shrink_to_fit(
+    vector* v)
+{
+    if(v->capacity > v->size)
+    {
+        v->capacity = v->size;
+        v->data = realloc(v->data, v->capacity * v->element_size);
+    }
+}
+
+iterator begin(
+    vector const* v)
+{
+    return (iterator){
+            .element = v->data,
+            .element_size = v->element_size,
+            };
+}
+
+iterator end(
+    vector const* v)
+{
+    return (iterator){
+            .element = v->data + v->element_size * v->size,
+            .element_size = v->element_size,
+            };
+}
+
+iterator at(
+    vector const* v,
+    size_t const index)
+{
+    return (iterator){
+            .element = v->data + v->element_size * index,
+            .element_size = v->element_size,
+            };
+}
+
+int compare(
+    iterator const a,
+    iterator const b)
+{
+    return a.element - b.element;
+}
+
+void increment(
+    iterator* i,
+    size_t const num_elements)
+{
+    i->element += i->element_size * num_elements;
+}
+
+void decrement(
+    iterator* i,
+    size_t const num_elements)
+{
+    i->element -= i->element_size * num_elements;
+}
+
+void* value(
+    iterator const i)
+{
+    return i.element;
+}
+
+void set(
+    iterator i,
+    void const* data)
+{
+    memcpy(i.element, data, i.element_size);
+}
