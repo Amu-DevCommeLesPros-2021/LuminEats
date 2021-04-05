a5c401c6bb8bf721c85b6157c4d43482ad6036fa
Thierry Seegers <surfintheusa@yahoo.com>
2021-04-05 13:03:23 +0200

    Add (empty) test program.

---
+++ test/main.c
@@ -0,0 +1,12 @@
+#include "test_harness/test_harness.h"
+
+// Valeurs pour le harnais de test spécifiques à ce programme.
+int const tests_total = 0;
+int const test_column_width = 40;
+
+int main()
+{
+
+
+    return 0;
+}
