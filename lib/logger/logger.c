0cc3a0099c4fd2bf67ef59ccfd7283b42cad0319
Thierry Seegers <surfintheusa@yahoo.com>
2021-04-06 14:02:44 +0200

    Add logging code.

--- lib/logger/logger.c
+++ lib/logger/logger.c
@@ -1,1 +1,36 @@
 #include "logger/logger.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <time.h>
+
+FILE *file;
+
+void lopen(char const* chemin)
+{
+    if(!file)
+    {
+        file = fopen(chemin, "a");
+    }
+}
+
+void llog(char const* format, ...)
+{
+    if(!file)
+    {
+        return;
+    }
+    
+    char time_s[100];
+    time_t now = time(0);
+    strftime(time_s, 100, "%Y-%m-%d %H:%M:%S.000", localtime(&now));
+    fprintf(file, "[%s] ", time_s);
+
+    va_list args;
+    va_start(args, format);
+    vfprintf(file, format, args);
+    va_end(args);
+
+    fprintf(file, "\n");
+    fflush(file);
+}
--- lib/logger/logger.h
+++ lib/logger/logger.h
@@ -1,1 +1,5 @@
 #pragma once
+
+void lopen(char const* chemin);
+
+void llog(char const* format, ...);
