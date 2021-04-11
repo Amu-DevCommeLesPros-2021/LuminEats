1d7f82d117e8f60d9d693b19c9cd2528dd3dc51e
Thierry Seegers <surfintheusa@yahoo.com>
2021-04-11 16:22:46 +0200

    Add option to specify path to database.

--- bin/main.c
+++ bin/main.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
-
 #include "ecrans.h"
 
+#include "db/db.h"
 #include "vector/vector.h"
 
-int main()
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
 {
+    char *db_folder = NULL;
+
+    int c;
+    while((c = getopt (argc, argv, "b:")) != -1)
+    {
+        switch(c)
+        {
+            case 'b':
+                db_folder = optarg;
+                break;
+            default:
+                abort();
+                break;
+        }
+    }
+
+    ouverture_db(db_folder);
+
     printf("\n*** Bienvenu sur LuminEats, la livraison à vitesse luminique ***\n");
 
     vector ecrans = make_vector(sizeof(ecran), 0, 2.0);
@@ -22,6 +42,8 @@ int main()
     destroy(&ecrans);
 
     printf("\n*** À très bientôt ! ***\n");
-    
+
+    fermeture_db(db_folder);
+
     return 0;
 }
